bffc6d830e3893fcab5566c78207d3664e9e4e79
code-lova <ebinuga@hotmail.com>
2025-12-10 00:19:19 +0100

    doc: deploy app

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -40,7 +40,7 @@ export default function Home() {
   }, []);
 
   return (
-    <main className="bg-[url('images/bg-main.svg')] bg-cover">
+    <main className="bg-[url('/images/bg-main.svg')] bg-cover">
       <Navbar />
       <section className="main-section">
         <div className="page-heading py-8">
